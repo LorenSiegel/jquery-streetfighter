8bb10668660aafc4341afe17b2f78b81b0645ed1
Loren Siegel <lorensiegel@Lorens-iMac.local>
2014-06-08 10:59:02 -0500

    Ryu looking cool and with instructions

--- js/app.js
+++ js/app.js
@@ -2,13 +2,19 @@ $(document).ready(function() {
 $(".ryu").mouseenter(function(){
 	$(".ryu-still").hide();
 	$(".ryu-ready").show();
+	$(".ryu-cool").hide();
+	$(".ryu-throwing").hide();
 	})
 	.mouseleave(function(){
 		$(".ryu-ready").hide();
 		$(".ryu-still").show();
+		$(".ryu-cool").hide();
+		$(".ryu-throwing").hide();
 	})
 	.mousedown(function(){
 		playHadouken();
+		$(".ryu-cool").hide();
+		$(".ryu-still").hide();
 		$(".ryu-ready").hide();
 		$(".ryu-throwing").show();
 		$(".hadouken").show();
@@ -25,9 +31,25 @@ $(".ryu").mouseenter(function(){
 	.mouseup (function(){
 		$(".ryu-throwing").hide();
 		$(".ryu-ready").show();
-		// ryu goes back to his ready position
+		$(".ryu-cool").hide();
+		$(".ryu-still").hide();
+	});
+	$(document).keydown(function(){
+		if(event.which === 88) {
+			$(".ryu-still").hide();
+			$(".ryu-ready").hide();
+			$(".ryu-throwing").hide();
+			$(".ryu-cool").show();
+		}
+	})
+	.keyup(function(){
+		$(".ryu-cool").hide();
+		$(".ryu-ready").hide();
+		$(".ryu-throwing").hide();
+		$(".ryu-still").show();
 	});
 });	
+
 function playHadouken () {
 	$("#hadouken-sound") [0].volume = 0.5;
 	$("#hadouken-sound") [0].load();
